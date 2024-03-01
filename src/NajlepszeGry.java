ca1f43452be54e61d1249d4f3113f354471a3b1e
Ka <@gmail.com>
2024-03-01 09:56:34 +0100

    Initial commit

---
+++ src/NajlepszeGry.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+public class NajlepszeGry {
+
+    public static void main(String[] args) {
+
+// System.out.print("\"Minecraft \" (bo napisany w Javie:)");
+//        System.out.print ("\" WOT \"");
+//
+//        System.out.print("\"CS 1.6\"");
+//
+//System.out.print("\"Dying Light\"\n");
+
+
+//        System.out.println("Adam Rzemiński");
+//        System.out.println("Siołkowa 547");
+//        System.out.println("Grybów 33-330");
+//        System.out.println("780 104 393");
+
+
+        for (int i = 0; i < 7; i++ ) {
+            for (int j = 0; j < 7; j++ ) {
+                
+            }
+        }
+
+
+//        char letter;
+//        letter = 'A';
+//        System.out.println(letter);
+
+
+//        int liczbaPomieszczen, pomieszczenia3x4, pomieszczenia2x3, pomieszczenia2x2, powPom3x4, powPom2x3, powPom2x2, powMieszkania;
+//        pomieszczenia3x4 = 2;
+//        pomieszczenia2x2 = 1;
+//        pomieszczenia2x3 = 1;
+//
+//        powPom3x4 = 3*4;
+//        powPom2x2 = 2*2;
+//        powPom2x3 = 2*3;
+//
+//        powMieszkania = pomieszczenia2x2*powPom2x2 + pomieszczenia2x3*powPom2x3 + pomieszczenia3x4*powPom3x4;
+//        System.out.println(pomieszczenia3x4);
+//        System.out.println(powPom3x4);
+//        System.out.println();
+//
+//        System.out.println(pomieszczenia2x2);
+//        System.out.println(powPom2x3);
+//        System.out.println();
+//
+//        System.out.println(pomieszczenia2x2);
+//        System.out.println(powPom2x2);
+//        System.out.println();
+//
+//        System.out.println(powMieszkania);
+//
+//
+//        double powNaOsobe;
+//        powNaOsobe = powMieszkania/4;
+//        System.out.println(powNaOsobe);
+
+
+//        double wartoscSprzedazy, regionMazowiecki, wartSprzedMazowieckie;
+//        wartoscSprzedazy = 4600000;
+//        regionMazowiecki = 0.62;
+//        wartSprzedMazowieckie = regionMazowiecki * wartoscSprzedazy;
+
+
+//        int  pom1szer, pom2szer, pom3szer, pom4szer, pom1dl, pom2dl, pom3dl, pom4dl, pom1pow, pom2pow, pom3pow, pom4pow, powMieszkania;
+//
+//        Scanner klawiatura = new Scanner(System.in);
+//
+//        System.out.println("Podaj długość i szerokość pokoju 1 w metrach:");
+//        pom1dl = klawiatura.nextInt();
+//        pom1szer = klawiatura.nextInt();
+//
+//        System.out.println("Podaj długość i szerokość pokoju 2 w metrach:");
+//        pom2dl = klawiatura.nextInt();
+//        pom2szer = klawiatura.nextInt();
+//
+//        System.out.println("Podaj długość i szerokość pokoju 3 w metrach:");
+//        pom3dl = klawiatura.nextInt();
+//        pom3szer = klawiatura.nextInt();
+//
+//        System.out.println("Podaj długość i szerokość pokoju 4 w metrach:");
+//        pom4dl = klawiatura.nextInt();
+//        pom4szer = klawiatura.nextInt();
+//
+//        pom1pow = pom1szer * pom1dl;
+//        pom2pow = pom2szer * pom2dl;
+//        pom3pow = pom3szer * pom3dl;
+//        pom4pow = pom4szer * pom4dl;
+//
+//        powMieszkania =pom1pow + pom2pow + pom3pow + pom4pow;
+//
+//
+//
+//        System.out.println(powMieszkania);
+//
+//
+//        double powNaOsobe;
+//        powNaOsobe = powMieszkania/4;
+//        System.out.println(powNaOsobe);
+    }
+}
